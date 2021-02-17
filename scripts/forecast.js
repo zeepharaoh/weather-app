d5034cae04219ce130f1906272be8c11f253fe5c
Zeyad Gomaa <zeyad.mohsen.s@gmail.com>
2021-02-17 22:57:58 +0100

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,7 @@
 const key = 'CFkpn7Y18gjI4AV2Ckfszui9JqhaHeCY';
 // ! Get weather information
 const getWeather = async id => {
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${id}?apikey=${key}`;
   const response = await fetch(base + query);
   const data = await response.json();
@@ -10,7 +10,7 @@ const getWeather = async id => {
 
 // !Get city information
 const getCity = async city => {
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey= ${key}&q=${city}`;
 
   const response = await fetch(base + query);
